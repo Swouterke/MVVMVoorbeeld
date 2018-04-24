98594bc6cbdfdb0504fdf2d98a3d39b5af6e86c8
Swouterke <wvercruysse45@gmail.com>
2018-04-24 11:23:21 +0200

    Uitgewerkt BindingsMVVM

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,15 @@ namespace MVVMVoorbeeld
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Model.TekstMetOpmaak mijnTekst = new Model.TekstMetOpmaak("Tik hier uw tekst...", false, false);
+            ViewModel.TekstMetOpmaakVM vm = new ViewModel.TekstMetOpmaakVM(mijnTekst);
+            View.TextBoxView mijnTextBoxView = new View.TextBoxView();
+
+            mijnTextBoxView.Closing += vm.OnWindowClosing;
+            mijnTextBoxView.DataContext = vm;
+            mijnTextBoxView.Show();
+        }
     }
 }
--- View/TextBoxView.xaml.cs
+++ View/TextBoxView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MVVMVoorbeeld
+namespace MVVMVoorbeeld.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for TextBoxView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TextBoxView : Window
     {
-        public MainWindow()
+        public TextBoxView()
         {
             InitializeComponent();
         }
